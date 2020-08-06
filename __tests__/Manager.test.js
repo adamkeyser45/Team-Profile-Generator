ce5286a8f5f9a7f939eae77cc9e451136bd0c055
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-08-06 14:09:51 -0500

    added tests and code for Manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,14 @@
 const Manager = require('../lib/Manager');
+const { TestScheduler } = require('jest');
+
+test('checks Manager office number', () => {
+    const manager = new Manager('Jim', 1002, 'jim@gmail.com', 24);
+
+    expect(manager.office).toEqual(expect.any(Number));
+});
+
+test('check getRole to return Manager', () => {
+    const manager = new Manager('Jim', 1002, 'jim@gmail.com', 24);
+
+    expect(manager.getRole()).toBe("Manager");
+});
