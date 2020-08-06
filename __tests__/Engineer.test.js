ad1a77dbfefcb4298c5b27d598a317f760965aeb
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-08-06 15:00:07 -0500

    added tests and code for Engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require('../lib/Engineer');
+
+test('checks Engineer github', () => {
+    const engineer = new Engineer('Jim', 1002, 'jim@gmail.com', 'jimgithub');
+
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+test('check getRole to return Engineer', () => {
+    const engineer = new Engineer('Jim', 1002, 'jim@gmail.com', 'jimgithub');
+
+    expect(engineer.getRole()).toBe("Engineer");
+});
+
+test('checks the retrieval of the github account', () => {
+    const engineer = new Engineer('Jim', 1002, 'jim@gmail.com', 'jimgithub');
+
+    expect(engineer.getGithub()).toBe('jimgithub');
+});
