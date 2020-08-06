8a5c7bff3ed5d612c908be65ab61e571fb2882c9
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-08-06 16:00:09 -0500

    created first set of inquirer prompts for team managers

--- Index.js
+++ Index.js
@@ -1,3 +1,67 @@
 const inquirer = require('inquirer');
 const generatePage = require('./utils/generatePage.js');
-const { writeFile, copyFile } = require('./utils/generate-site.js');
+const { writeFile, copyFile } = require('./utils/generatePage.js');
+
+const promptUserManager = () => {
+    console.log("Welcocme to the Dev Team Page Builder! Follow the prompts below to build your team.");
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is the team manager's name?",
+            validate: managerName => {
+                if (managerName) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "What is the team manager's ID#?",
+            validate: managerId => {
+                if (managerId > 0) {
+                    return true;
+                } else {
+                    console.log("Please enter a number that is greater than 0!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email address?",
+            validate: managerEmail => {
+                if (managerEmail) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's email!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerOffice',
+            message: "What is the team manager's office number?",
+            validate: managerOffice => {
+                if (managerOffice > 0) {
+                    return true;
+                } else {
+                    console.log("Please enter a number that is greater than 0!");
+                    return false;
+                }
+            }
+        }
+    ])
+};
+
+promptUserManager()
+    .then(teamData => {
+        console.log(teamData);
+    });
+
