686bcf2eb2b96fdf231dce26e180ec90c9f234f8
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-08-06 15:09:17 -0500

    added tests and code for Intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
 const Intern = require('../lib/Intern');
+
+test('checks Intern school', () => {
+    const intern = new Intern('Jim', 1002, 'jim@gmail.com', 'State University');
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+test('check getRole to return Intern', () => {
+    const intern = new Intern('Jim', 1002, 'jim@gmail.com', 'State University');
+
+    expect(intern.getRole()).toBe("Intern");
+});
+
+test('checks the retrieval of the school', () => {
+    const intern = new Intern('Jim', 1002, 'jim@gmail.com', 'State University');
+
+    expect(intern.getSchool()).toBe('State University');
+});
